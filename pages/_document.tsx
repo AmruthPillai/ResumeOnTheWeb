5b0ff571ff59e502cdbe3e83e3a1a9c3b41bdfe0
Amruth Pillai <im.amruth@gmail.com>
2022-07-24 12:03:10 +0200

    Fix Google Web Fonts loading issue

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => (
+  <Html lang="en">
+    <Head>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@400;600&display=swap" />
+    </Head>
+
+    <body>
+      <Main />
+
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
